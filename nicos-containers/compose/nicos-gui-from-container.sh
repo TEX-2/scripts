55f8b2f242f0729860417c14f3a98bee3383316a
Kirill <pshcyrill@mail.ru>
2019-12-10 15:21:30 +0300

    set INSTRUMENT

--- nicos-containers/compose/nicos-gui-from-container.sh
+++ nicos-containers/compose/nicos-gui-from-container.sh
@@ -10,5 +10,7 @@ PATH_TO_INSTRUMENT=${DIR}/nicos_pnpi
 PATH_TO_DATA=${DIR}/data
 PATH_TO_LOG=${DIR}/log
 
+INSTRUMENT=nicos_pnpi.tex2
+
 xhost +local:
-docker run -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PATH_TO_INSTRUMENT}:/usr/src/nicos/nicos_pnpi -v ${PATH_TO_DATA}:/data -v ${PATH_TO_LOG}:/log --network host -ti ${TAG} ${BIN}
+docker run -e INSTRUMENT=${INSTRUMENT} -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PATH_TO_INSTRUMENT}:/usr/src/nicos/nicos_pnpi -v ${PATH_TO_DATA}:/data -v ${PATH_TO_LOG}:/log --network host -ti ${TAG} ${BIN}
