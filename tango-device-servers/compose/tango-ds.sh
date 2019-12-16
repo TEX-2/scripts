55f8f681fbcd59fa7a6b7a7fb6d0eacc822cc5d0
Kirill <pshcyrill@mail.ru>
2019-12-16 19:25:45 +0300

    set wait PhyMotionDevice for ip addr host

--- tango-device-servers/compose/tango-ds.sh
+++ tango-device-servers/compose/tango-ds.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+WAIT_HOST=HOST=10.10.7.10   # PhyMotion Deivce IP addr
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
 
 case $1 in
-	start)
-		docker-compose up -d
-	;;	
-	stop)
-		docker-compose down
-	;;
-	restart)
-		docker-compose down && docker-compose up -d
-	;;	
+    start)
+	while [[ 1==1 ]]
+	do
+	    ping -c 1 ${HOST} && break
+	done
+	docker-compose up -d
+    ;;	
+    stop)
+	docker-compose down
+    ;;
+    restart)
+	docker-compose down && docker-compose up -d
+    ;;	
 esac
