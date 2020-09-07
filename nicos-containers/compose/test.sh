fa66198efbde15396bd3300eea6bc542991f61c0
Kirill <pshcyrill@mail.ru>
2020-09-08 00:19:18 +0300

    add config direcotry

--- nicos-containers/compose/test.sh
+++ nicos-containers/compose/test.sh
@@ -7,4 +7,4 @@ cd ${DIR}
 
 PATH_TO_INSTRUMENT=${DIR}/nicos_pnpi
 xhost +local:
-docker run -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PATH_TO_INSTRUMENT}:/usr/src/nicos/nicos_pnpi --network host -ti ${TAG} ${BIN}
+docker run -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PATH_TO_INSTRUMENT}:/usr/src/nicos/nicos_pnpi  -v ${DIR}/data:/data -v ${DIR}/log:/log --network host -ti ${TAG} ${BIN}
